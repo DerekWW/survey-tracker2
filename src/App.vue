76ceaffad720e7f4e6c726c3f13c8610a3854986
Derek Wierson <derek.wierson@gmail.com>
2017-03-09 22:38:28 -0800

    add firebase

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view></router-view>
+    <router-view :info='users'></router-view>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyDVvtSK4fZ6htviqyvwUsMAQ-MYbPkgCEk",
+  authDomain: "survey-tracker-3d376.firebaseapp.com",
+  databaseURL: "https://survey-tracker-3d376.firebaseio.com",
+  storageBucket: "survey-tracker-3d376.appspot.com",
+  messagingSenderId: "1018029921808"
+};
+
+let app = firebase.initializeApp(config);
+let db = app.database();
+
+let users1 = db.ref('users')
+
 export default {
   name: 'app',
+  firebase: {
+    users: users1
+  },
+  components: {
+
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,22 +1,23 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/* eslint-disable */
+import VueFire from 'vuefire';
+import Firebase from 'firebase';
+import VueResource from 'vue-resource'
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
-let VueResource = require('vue-resource');
-let VueFire = require('vuefire')
-let Firebase = require('firebase')
+
 
 // explicit installation required in module environments
-Vue.use(VueFire)
+Vue.use(VueFire);
 Vue.use(VueResource);
 
 
 Vue.config.productionTip = false;
 
-let firebaseApp = firebase.initializeApp({ ... })
-let db = firebaseApp.database()
+
 
 /* eslint-disable no-new */
 new Vue({
